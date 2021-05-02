b71c71547ac7643bfee6d48986f3389227ee6cf9
vincentcollis <vinsanity0050@gmail.com>
2021-05-02 13:04:57 -0400

    js edits

--- Assets/Javascript/todos.js
+++ Assets/Javascript/todos.js
@@ -7,3 +7,14 @@ $("ul").on("click", "li",function () {
     $(this).toggleClass("completed")
 });
 
+// Click on X to delete TO DO
+// Inside the ul, set click event on spans
+$("ul").on("click","span",function (event) {
+    // Fades out then removes the li
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    // Stops code from bubbling
+    event.stopPropagation();
+});
+
